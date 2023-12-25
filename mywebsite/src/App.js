9933e5c7537161e3b0c133c64e8e8283da3fb7ec
zou-y-t <1354410159@qq.com>
2023-12-25 20:44:36 +0800

    merry christmas3.0

--- mywebsite/src/App.js
+++ mywebsite/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  useEffect(() => {
+    // 在组件挂载后生成雪花
+    for (let i = 0; i < 200; i++) {
+      const snowflake = document.createElement('i');
+      snowflake.style.left = Math.random() * window.innerWidth + 'px';
+      snowflake.style.animationDuration = Math.random() * 3 + 2 + 's'; // 随机下落时间
+      snowflake.style.opacity = Math.random(); // 随机透明度
+      snowflake.style.transform = `scale(${Math.random()})`; // 随机大小
+      snowflake.classList.add('snowflake');
+      document.getElementById('snowflakes').appendChild(snowflake);
+    }
+  }, []);
   return (
     <>
-      <body style={{backgroundColor: 'black'}}>
+      <body style={{backgroundColor: 'black'}} id="snowflakes" >
         <h5 style={{ color: 'white' }}>HELLO Nuannuan,</h5>
         <h1 style={{ color: 'white' }}>MERRY CHRISTMAS!</h1>
         <div className="tree">
