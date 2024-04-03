b075a15abd9c0bfe89c695e655e63c91f82407fc
zou-y-t <1354410159@qq.com>
2024-04-04 02:44:32 +0800

    moderate the 3d scene in the contest home page

--- mywebsite/src/app/ContestSite/contestHome.js
+++ mywebsite/src/app/ContestSite/contestHome.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 function ContestHome() {
     const ref = useRef(null);
@@ -10,6 +11,7 @@ function ContestHome() {
         const renderer = new THREE.WebGLRenderer();
 
         renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.shadowMap.enabled = true;//开启阴影
         ref.current.appendChild(renderer.domElement);
 
         const loader = new THREE.TextureLoader();
@@ -19,11 +21,67 @@ function ContestHome() {
         // const material = new THREE.MeshBasicMaterial({ map: loader.load('https://threejs.org/examples/textures/crate.gif') });
         const material = new THREE.MeshBasicMaterial({ map: loader.load('app.jpg') });
         const cube = new THREE.Mesh(geometry, material);
+        cube.position.y = 1; // Set the height of the cube from the ground
+        cube.castShadow = true;//产生阴影
+        cube.receiveShadow = true;//接受阴影
         scene.add(cube);
 
-        
+        //添加光源
+        {
+
+            //const color = 0xFFEEDD;
+            const color = 0xFFFFFF;
+            const intensity = 3;
+            const light = new THREE.DirectionalLight( color, intensity );
+            light.position.set( -1, 2, 4 );
+            light.castShadow = true;//产生阴影
+            scene.add( light );
+    
+        }
+
+        //添加控制器
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.update();
+
+        //添加地板
+        {
+            const planeSize = 40;
+
+            const loader = new THREE.TextureLoader();
+            const texture = loader.load( 'https://threejs.org/manual/examples/resources/images/checker.png' );
+            texture.wrapS = THREE.RepeatWrapping;
+            texture.wrapT = THREE.RepeatWrapping;
+            texture.magFilter = THREE.NearestFilter;
+            //texture.colorSpace = THREE.SRGBColorSpace;
+            const repeats = planeSize / 2;
+            texture.repeat.set( repeats, repeats );
+
+            const planeGeo = new THREE.PlaneGeometry( planeSize, planeSize );
+            const planeMat = new THREE.MeshPhongMaterial( {
+                map: texture,
+                side: THREE.DoubleSide,
+            } );
+            const mesh = new THREE.Mesh( planeGeo, planeMat );
+            mesh.rotation.x = Math.PI * - .5;
+            mesh.receiveShadow = true;//接受阴影
+            scene.add( mesh );
+        }
+
+        //添加背景
+        // const texture = loader.load([
+        // 'app.jpg', // 右面
+        // 'app.jpg', // 左面
+        // 'app.jpg', // 顶面
+        // 'app.jpg', // 底面
+        // 'app.jpg', // 前面
+        // 'app.jpg' // 后面
+        // ]);
+
+        // scene.background = texture;
+
 
         camera.position.z = 5;
+        camera.position.y = 2;
 
         const animate = function () {
             requestAnimationFrame(animate);
